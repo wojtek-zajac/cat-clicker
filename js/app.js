019c838631d0ab02759c6c82e53ab6e7327df22d
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-07-09 21:15:32 +0200

    Add second cat and cat names

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
-const cat = document.querySelector('.cat');
+const cat1 = document.querySelector('.cat1');
+const cat2 = document.querySelector('.cat2');
+const cat1Name = document.querySelector('.cat1Name');
+const cat2Name = document.querySelector('.cat2Name');
 let number = document.querySelector('.number');
 let click = 0;
+
+function displayCatsNames() {
+    cat1Name.innerHTML = "Grumpy Cat";
+    cat2Name.innerHTML = "Sneezy Cat";
+}
 
 function countClicks() {
     click++;
@@ -11,4 +19,9 @@ function updatePage() {
     number.innerHTML = click;
 }
 
-cat.addEventListener('click', countClicks);
+window.onload = () => {
+    displayCatsNames();
+}
+
+cat1.addEventListener('click', countClicks);
+cat2.addEventListener('click', countClicks);

f8348b6e22e067c37f39b3dfc163d828b0762df8
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-07-10 00:58:51 +0200

    Add multiple cats list and sound

--- js/app.js
+++ js/app.js
@@ -1,27 +1,91 @@
-const cat1 = document.querySelector('.cat1');
-const cat2 = document.querySelector('.cat2');
-const cat1Name = document.querySelector('.cat1Name');
-const cat2Name = document.querySelector('.cat2Name');
+const cat = document.querySelector('.catImage');
+const catName = document.querySelector('.catName');
+const catImageContainer = document.querySelector('.catImage');
+const catList = document.querySelector('.list');
+const meow = new Audio('./audio/meow3.mp3');
+let cats = ['cat1',
+            'cat2',
+            'cat3',
+            'cat4',
+            'cat5',
+            'cat6',
+            'cat7',
+            'cat8'
+        ];
 let number = document.querySelector('.number');
 let click = 0;
 
-function displayCatsNames() {
-    cat1Name.innerHTML = "Grumpy Cat";
-    cat2Name.innerHTML = "Sneezy Cat";
-}
+
+// Loop over the cats in our array
+for (let i = 0; i < cats.length; i++) {
+
+    // This is the cat we're on...
+    let cat = cats[i];
+
+    // We're creating a DOM element for the cat
+    let elem = document.createElement('div');
+    elem.textContent = cat;
+
+    // ... and when we click, push the given cat to the cantainer
+    elem.addEventListener('click', (function(catCopy) {
+        return function() {
+
+            // Clean the cat's container content first
+            cleanImages();
+            cleanClicks();
+
+            // Create an `img` element for a cat
+            let img = document.createElement('img');
+
+            // Set the element's attributes
+            img.setAttribute('alt', 'cat');
+            img.className += 'cat';
+            img.src = `./img/${catCopy}.jpg`;
+
+            // Display the image on screen
+            catImageContainer.appendChild(img);
+
+            // Display the name on screen
+            catName.innerHTML = catCopy;
+        };
+    })(cat));
+
+    // Create list of cats
+    catList.appendChild(elem);
+    
+};
+
 
 function countClicks() {
     click++;
-    updatePage();
+    updateClicks();
 }
 
-function updatePage() {
-    number.innerHTML = click;
+
+function updateClicks() {
+    if (click > 1) {
+        number.innerHTML = `${click} clicks`;
+    } else {
+        number.innerHTML = `${click} click`;
+    }
 }
 
-window.onload = () => {
-    displayCatsNames();
+
+function cleanClicks() {
+    click = 0;
+    number.innerHTML = '';
 }
 
-cat1.addEventListener('click', countClicks);
-cat2.addEventListener('click', countClicks);
+
+function meeow() {
+    meow.play();
+}
+
+
+function cleanImages() {
+    catImageContainer.removeChild(catImageContainer.childNodes[2]); 
+}
+
+
+cat.addEventListener('click', countClicks);
+cat.addEventListener('click', meeow);
